33a7393d99f751bac6f80e211865eb8f28bd1aed
RdzCore <daniel_rod_con@hotmail.com>
2021-03-17 16:18:28 -0600

    added firebase Config and implement logging by GitHub

---
+++ firebase/client.js
@@ -0,0 +1,36 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw60JAEHjKGq0Qt5ylGBPje8KtyDxytX8",
+  authDomain: "devtter-99384.firebaseapp.com",
+  projectId: "devtter-99384",
+  storageBucket: "devtter-99384.appspot.com",
+  messagingSenderId: "390662241085",
+  appId: "1:390662241085:web:c05ce0ea0419c6265fe354",
+  measurementId: "G-2NN6R2BGB4",
+};
+
+firebase.apps.length === 0 && firebase.initializeApp(firebaseConfig);
+
+const mapUserFromFirebase = (user) => {
+  const { displayName, email, photoURL } = user;
+  console.log("user", user);
+
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email,
+  };
+};
+
+export const onAuthStateChange = (onChange) => {
+  return firebase.auth().onAuthStateChanged((user) => {
+    const normalizedUser = mapUserFromFirebase(user);
+    onChange(normalizedUser);
+  });
+};
+
+export const loggingWithGitHub = () => {
+  const gitHubProvider = new firebase.auth.GithubAuthProvider();
+  return firebase.auth().signInWithPopup(gitHubProvider);
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 
 import Button from "../components/botton";
 
+import { loggingWithGitHub, onAuthStateChange } from "../firebase/client";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChange(setUser);
+  }, []);
+  const handleClick = () => {
+    loggingWithGitHub()
+      .then((user) => {
+        const { avatar, url, username } = user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log("handleClick err", err);
+      });
+  };
   return (
     <div className={styles.wrapper}>
       <Head>
@@ -18,7 +37,15 @@ export default function Home() {
           <h2 className={styles.subtitle}>
             Talk About Developement With Developers...
           </h2>
-          <Button>Loggin With GitHub</Button>
+          {user === null && (
+            <Button onClick={handleClick}>Loggin With GitHub</Button>
+          )}
+          {user && user.avatar && (
+            <div>
+              <img src={user.avatar} />
+              <strong>{user.username}</strong>
+            </div>
+          )}
         </div>
       </div>
     </div>
