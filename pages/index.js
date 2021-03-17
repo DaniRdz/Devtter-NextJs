d2bf2b36f5f6de575a88e7208684665b121068a3
RdzCore <daniel_rod_con@hotmail.com>
2021-03-16 19:42:32 -0600

    added some styles for only mobile app

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import Head from "next/head";
-import Link from "next/link";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.wrapper}>
       <Head>
         <title>Devtter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className={styles.title}>Welcome to Devtter</h1>
-      <nav className={styles.nav}>
-        <Link href="/timeline">Time line</Link>
-      </nav>
+      <div className={styles.container}>
+        <h1>Devtter :D</h1>
+      </div>
     </div>
   );
 }
