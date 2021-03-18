216a4c722f584653b8ab4236c2e7b1e91461bf3e
RdzCore <daniel_rod_con@hotmail.com>
2021-03-18 16:41:32 -0600

    render some devteets

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+
 import AppLayout from "components/appLayout";
+import Avatar from "components/avatar";
+
+import styles from "styles/TimeLine.module.css";
 export default function Home() {
+  const [timeline, setTimeline] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/statuses/home_timeline")
+      .then((res) => res.json())
+      .then(setTimeline);
+  }, []);
+
   return (
     <AppLayout>
-      <h1>hello tweet</h1>
+      <header className={styles.header}>
+        <h2>Inicio</h2>
+      </header>
+      <section className={styles.timeLine}>
+        {timeline.map((deveet) => {
+          return (
+            <article key={deveet.id}>
+              <Avatar alt={deveet.username} src={deveet.avatar} />
+              <div>
+                <strong>{deveet.username}</strong>
+                <p>{deveet.message}</p>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+      <nav className={styles.nav}>holi</nav>
     </AppLayout>
   );
 }
