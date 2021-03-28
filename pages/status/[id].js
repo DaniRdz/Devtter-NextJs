7d55d57b01d9b87dea194054a77033f379b1a244
RdzCore <daniel_rod_con@hotmail.com>
2021-03-27 21:00:10 -0600

    used getInitialProps for data feching in status id

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,3 +1,18 @@
-export default function DeveetPage() {
-  return <>Hola bandamax</>;
+import Deveet from "components/deveet";
+
+export default function DeveetPage(props) {
+  return <Deveet {...props} />;
 }
+
+DeveetPage.getInitialProps = (context) => {
+  const { query, res } = context;
+  const { id } = query;
+  return fetch(`http://localhost:3000/api/deveets/${id}`).then((apiRes) => {
+    if (apiRes.ok) {
+      return apiRes.json();
+    }
+    if (res) {
+      res.writeHead(301, { Location: "/home" }).end();
+    }
+  });
+};
